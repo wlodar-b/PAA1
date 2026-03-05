6f088acd4d361442a35ac24d9bc34d24f6f5b78f
wlodar-b <wlodarcykb16@gmail.com>
2026-03-05 15:43:15 +0100

    Rozpoczecie projektu

---
+++ utils.hpp
@@ -0,0 +1,7 @@
+// Zabezpieczenie przed wielokrotnym dołączeniem tego samego pliku
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <iostream> // bibliotegka do wypisywania tekstu na ekran
+
+// Szablon funkcji - pozwala funkcji przyjmować tablice dowonlego typu 
